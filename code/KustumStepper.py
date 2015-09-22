ccd1335c8c5948f76bcacad25378d05874f3ab1b
joaquincasanova <joaquin.casanova@gmail.com>
2015-09-22 18:51:55 +0000

    layouts, bresenham

--- code/KustumStepper.py
+++ code/KustumStepper.py
@@ -22,8 +22,8 @@ global ZRelay
 global XLimitSwitch
 global YLimitSwitch
 
-global DeltaX
-global DeltaY
+global XDelta
+global YDelta
 
 global XDim 
 global YDim 
@@ -56,9 +56,23 @@ YDim = 1e-2 #m
 X = 0
 Y = 0
 
-#def XHome(channel):
+def XHome(channel):
+    print("XLimit")
+    Step(0, 1)
+    if GPIO.input(XLimitSwitch):
+        pass
+    else:
+        Step(0,-1)
+        Step(0,-1)
 
-#def YHome(channel):
+def YHome(channel):
+    print("YLimit")
+    Step(1, 1)
+    if GPIO.input(YLimitSwitch):
+        pass
+    else:
+        Step(1,-1)
+        Step(1,-1)
 
 GPIO.setup(XMotor1, GPIO.OUT)
 GPIO.setup(XMotor2, GPIO.OUT)
@@ -76,8 +90,8 @@ GPIO.setup(ZRelay, GPIO.OUT)
 GPIO.setup(XLimitSwitch, GPIO.IN)
 GPIO.setup(YLimitSwitch, GPIO.IN)
 
-#GPIO.add_event_detect(XLimitSwitch, GPIO.RISING, callback=XHome)
-#GPIO.add_event_detect(YLimitSwitch, GPIO.RISING, callback=YHome)
+#GPIO.add_event_detect(XLimitSwitch, GPIO.FALLING, callback=XHome)
+#GPIO.add_event_detect(YLimitSwitch, GPIO.FALLING, callback=YHome)
 
 def Step(axis, dir):
     if axis==0:
@@ -168,58 +182,66 @@ def Step(axis, dir):
             GPIO.output(YMotor4,0)
             time.sleep(Delay)
 
-def Travel(Xprime, Yprime):
+def Travel(X1, Y1):
     global X
     global Y
     X0=X
     Y0=Y
-    XDistance=XPrime-X
-    YDistance=YPrime-Y
+    XDistance=X1-X0
+    YDistance=Y1-Y0
     XSteps = np.round(abs(XDistance)/XDelta)
     YSteps = np.round(abs(YDistance)/YDelta)
     XDir = np.sign(XDistance)
     YDir = np.sign(YDistance)
+
+    steep = (YSteps) > (XSteps)
     
     print 'XSteps ', XSteps
     print 'YSteps ', YSteps
-    if XSteps = 0:
+    if XSteps == 0:
         i = 0
         print 'Step Y'
         while i<YSteps:
             Step(1, YDir)
             i=i+1
-            Y=Y+YStep*YDir
-    elif YSteps = 0:
+            Y=Y+YDelta*YDir
+    elif YSteps == 0:
         print 'Step X'        
         i = 0
         while i<XSteps:
             Step(0, XDir)
             i=i+1
-            X=X+XStep*XDir        
+            X=X+XDelta*XDir        
     else:
         Slope = YDistance/YDistance
-        if abs(Slope)<=0.5:
+        if abs(Slope)<=1:
             i=0
             while i<XSteps:
                 Step(0, XDir)
+                print "Step X"
                 i=i+1
-                X=X+XStep*XDir                
+                X=X+XDelta*XDir                
                 Yi = Y
-                Yi1 = Y+YStep*Ydir
-                if abs((Yi1-Y0)-Slope*(X-X0))<abs((Yi-Y0)-Slope*(X-X0)):
+                Yi1 = Y+YDelta*YDir
+                if abs((Yi1-Y0)-Slope*(X-X0))<YDelta/2:
                     Y=Yi1
+                    Step(1, YDir)
+                    print "Step Y"
                 else:
                     Y=Yi
         else:
             i=0
             while i<YSteps:
                 Step(1, YDir)
+                print "Step Y"
                 i=i+1
-                Y=Y+YStep*YDir                
+                Y=Y+YDelta*YDir                
                 Xi = X
-                Xi1 = X+XStep*Xdir
-                if abs((Xi1-X0)-1/Slope*(Y-Y0))<abs((Xi-X0)-1/Slope*(Y-Y0)):
+                Xi1 = X+XDelta*XDir
+                if abs((Xi1-X0)-1/Slope*(Y-Y0))<XDelta/2:
                     X=Xi1
+                    Step(0, XDir)
+                    print "Step X"
                 else:
                     X=Xi
 
@@ -235,9 +257,29 @@ def ZSolenoid(DurationZ,DurationSpindle):
     time.sleep((DurationZ-DurationSpindle)/2)
     GPIO.output(ZRelay,0)
 
-Travel(.1,.1)
-ZSolenoid(1,.5):
-Travel(0,0)
-        
+def Home():
+    while not GPIO.input(XLimitSwitch):
+        Step(0,-1)
+        print("Step")
+
+    while not GPIO.input(YLimitSwitch):
+        Step(1,-1)
+        print("Step")
+
+    global X
+    X=0
+    global Y
+    Y=0
+
+#Home()
+Travel(.5,.25)
+
+Travel(-.25,-.5)
+
+for i in range(0,20):
+    ZSolenoid(10,5)
+    time.sleep(10)
+
+#Home()
 GPIO.cleanup()
 quit()
