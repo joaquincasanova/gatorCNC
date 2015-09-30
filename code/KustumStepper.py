af2b308c04ed7c0a98395f8a84fc827cfe06cfde
joaquincasanova <joaquin.casanova@gmail.com>
2015-09-30 16:48:07 +0000

    gcode

--- code/KustumStepper.py
+++ code/KustumStepper.py
@@ -30,6 +30,7 @@ global YDim
 
 global X 
 global Y 
+global Z 
 
 XMotor1 = 7
 XMotor2 = 11
@@ -182,11 +183,14 @@ def Step(axis, dir):
             GPIO.output(YMotor4,0)
             time.sleep(Delay)
 
-def Travel(X1, Y1):
+def Travel(X1, Y1, Z1):
     global X
     global Y
+    global Z
+
     X0=X
     Y0=Y
+    Z0=Z
     XDistance=X1-X0
     YDistance=Y1-Y0
     XSteps = np.round(abs(XDistance)/XDelta)
@@ -195,7 +199,12 @@ def Travel(X1, Y1):
     YDir = np.sign(YDistance)
 
     steep = (YSteps) > (XSteps)
-    
+    if Z1>0:
+        GPIO.output(ZRelay,True)
+    else:
+        GPIO.output(ZRelay,False)
+    Z=Z1
+
     print 'XSteps ', XSteps
     print 'YSteps ', YSteps
     if XSteps == 0:
@@ -213,7 +222,7 @@ def Travel(X1, Y1):
             i=i+1
             X=X+XDelta*XDir        
     else:
-        Slope = YDistance/YDistance
+        Slope = YDistance/XDistance
         if abs(Slope)<=1:
             i=0
             while i<XSteps:
@@ -245,17 +254,88 @@ def Travel(X1, Y1):
                 else:
                     X=Xi
 
-def Spindle(DurationSpindle):
-    GPIO.output(SpindleRelay,1)
-    time.sleep(DurationSpindle)
-    GPIO.output(SpindleRelay,0)
+def Arc(lines):
+    old_x_pos=x_pos;
+    old_y_pos=y_pos;
+    
+    [x_pos,y_pos]=XYposition(lines);
+    [i_pos,j_pos]=IJposition(lines);
+    
+    xcenter=old_x_pos+i_pos;   #center of the circle for interpolation
+    ycenter=old_y_pos+j_pos;
+           
+           
+    Dx=x_pos-xcenter;
+    Dy=y_pos-ycenter;      #vector [Dx,Dy] points from the circle center to the new position
+           
+    r=sqrt(i_pos**2+j_pos**2);   # radius of the circle
+    
+    e1=[-i_pos,-j_pos]; #pointing from center to current position
+    if (lines[0:3]=='G02'): #clockwise
+        e2=[e1[1],-e1[0]];      #perpendicular to e1. e2 and e1 forms x-y system (clockwise)
+    else:                   #counterclockwise
+        e2=[-e1[1],e1[0]];      #perpendicular to e1. e1 and e2 forms x-y system (counterclockwise)
+        
+            #[Dx,Dy]=e1*cos(theta)+e2*sin(theta), theta is the open angle
+        
+    costheta=(Dx*e1[0]+Dy*e1[1])/r**2;
+    sintheta=(Dx*e2[0]+Dy*e2[1])/r**2;        #theta is the angule spanned by the circular interpolation curve
+               
+    if costheta>1:  # there will always be some numerical errors! Make sure abs(costheta)<=1
+        costheta=1;
+    elif costheta<-1:
+        costheta=-1;
+ 
+    theta=arccos(costheta);
+    if sintheta<0:
+        theta=2.0*pi-theta;
+        
+    no_step=int(round(r*theta/dx/5.0));   # number of point for the circular interpolation
+    
+    for i in range(1,no_step+1):
+        tmp_theta=i*theta/no_step;
+        tmp_x_pos=xcenter+e1[0]*cos(tmp_theta)+e2[0]*sin(tmp_theta);
+        tmp_y_pos=ycenter+e1[1]*cos(tmp_theta)+e2[1]*sin(tmp_theta);
+        moveto(MX,tmp_x_pos,dx,MY, tmp_y_pos,dy,speed,True);
+       
 
-def ZSolenoid(DurationZ,DurationSpindle):
-    GPIO.output(ZRelay,1)
-    time.sleep((DurationZ-DurationSpindle)/2)
-    Spindle(DurationSpindle)
-    time.sleep((DurationZ-DurationSpindle)/2)
-    GPIO.output(ZRelay,0)
+def XYposition(lines):
+    #given a movement command line, return the X Y position
+    xchar_loc=lines.index('X')
+    i=xchar_loc+1
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1
+    x_pos=float(lines[xchar_loc+1:i])    
+   
+    ychar_loc=lines.index('Y')
+    i=ychar_loc+1
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1
+    y_pos=float(lines[ychar_loc+1:i])    
+   
+    zchar_loc=lines.index('Z')
+    i=zchar_loc+1
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1
+    z_pos=float(lines[zchar_loc+1:i])    
+ 
+    return x_pos,y_pos,z_pos
+
+def IJposition(lines):
+    #given a G02 or G03 movement command line, return the I J position
+    ichar_loc=lines.index('I');
+    i=ichar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    i_pos=float(lines[ichar_loc+1:i]);    
+   
+    jchar_loc=lines.index('J');
+    i=jchar_loc+1;
+    while (47<ord(lines[i])<58)|(lines[i]=='.')|(lines[i]=='-'):
+        i+=1;
+    j_pos=float(lines[jchar_loc+1:i]);    
+ 
+    return i_pos,j_pos;
 
 def Home():
     while not GPIO.input(XLimitSwitch):
@@ -270,16 +350,53 @@ def Home():
     X=0
     global Y
     Y=0
+    GPIO.output(ZRelay,False)
+    global Z
+    Z=0
 
-#Home()
-Travel(.5,.25)
+filename = sys.argv[1])
+try:#read and execute G code
+    for lines in open(filename,'r'):
+        if lines==[]:
+            1 #blank lines
+        elif lines[0:3]=='G90':
+            print 'start'
+           
+        elif lines[0:3]=='G20':# working in inch
+            dx/=25.4
+            dy/=25.4
+            print 'Working in inch'
+             
+        elif lines[0:3]=='G21':# working in mm
+            print 'Working in mm'  
+           
+        elif lines[0:3]=='M05':
+            GPIO.output(SpindleRelay,False)
+            print 'Laser turned off'
+           
+        elif lines[0:3]=='M03':
+            GPIO.output(SpindleRelay,True)
+            print 'Laser turned on'
 
-Travel(-.25,-.5)
+        elif lines[0:3]=='G04':#dwell
+ 
+        elif lines[0:3]=='M02':
+            GPIO.output(SpindleRelay,False)
+            print 'finished. shuting down'
+            break
+        elif (lines[0:3]=='G1F')|(lines[0:4]=='G1 F'):
+            1#do nothing
+        elif (lines[0:3]=='G0 ')|(lines[0:3]=='G1 ')|(lines[0:3]=='G01'):#|(lines[0:3]=='G02')|(lines[0:3]=='G03'):
+            #linear engraving movement
+            [x_pos,y_pos,zpos]=XYposition(lines)
+            if (lines[0:3]=='G0 '):
+                Travel(x_pos,y_pos,z_pos)
+            else:
+                Travel(x_pos,y_pos,z_pos)          
+               
+        elif (lines[0:3]=='G02')|(lines[0:3]=='G03'): #circular interpolation
 
-for i in range(0,20):
-    ZSolenoid(10,5)
-    time.sleep(10)
-
-#Home()
+Home()
+time.sleep(10)
 GPIO.cleanup()
 quit()
